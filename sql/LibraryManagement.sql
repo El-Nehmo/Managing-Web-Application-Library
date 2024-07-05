69c0a584f8453cc2ce46686c2a4e343e263c72d5
Néhémie Mubenga Maweja <132246535+N3h3mya@users.noreply.github.com>
2024-07-05 13:37:53 +0200

    Update LibraryManagement.sql
    adding the member table

--- sql/LibraryManagement.sql
+++ sql/LibraryManagement.sql
@@ -1,1 +1,12 @@
 CREATE DATABASE LibraryManagement;
+
+CREATE TABLE Member (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(100),
+    prenom VARCHAR(100),
+    adresse VARCHAR(255),
+    telephone VARCHAR(15),
+    email VARCHAR(100),
+    date_inscription DATE,
+    amende DECIMAL(10, 2)
+);
