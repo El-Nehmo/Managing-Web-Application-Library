ecb2242033479a330b0e4ca2c54aa4d8e0631bdd
Néhémie Mubenga Maweja <132246535+N3h3mya@users.noreply.github.com>
2024-07-09 19:05:35 +0200

    Update db_connection.php
    fixing syntax issues

--- php/db_connection.php
+++ php/db_connection.php
@@ -4,9 +4,15 @@ $db_name = "LibraryManagement";
 $username = "root";
 $password = "";
 
-try{
-    $pdo = new PDO("mysql:host=$host; dbname = $db_name", $username, $password);
-}catch(PDOException $e){
-    echo "Erreur de connection: ".$e->getMessage();
+try {
+    // Correction de l'espace entre 'dbname' et '='
+    $pdo = new PDO("mysql:host=$host;dbname=$db_name", $username, $password);
+    // Configure PDO pour qu'il lance des exceptions en cas d'erreurs
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // Ligne de débogage pour confirmer la connexion
+    // echo "Connexion réussie !"; 
+} catch(PDOException $e) {
+    echo "Erreur de connexion: " . $e->getMessage();
+    die();
 }
 ?>
