7e787835536dc2b466b20d52ec5c86e190f9d182
Néhémie Mubenga Maweja <132246535+N3h3mya@users.noreply.github.com>
2024-07-12 15:51:36 +0200

    Update dashboard.php
    Added functionality to retrieve the user's recent history

--- php/dashboard.php
+++ php/dashboard.php
@@ -14,5 +14,12 @@ $sql = "SELECT * FROM Member WHERE id = ?";
 $stmt = $pdo->prepare($sql);
 $stmt->execute([$user_id]);
 $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+//Récupération de l'histoire de l'utilisateur
+$sql_history = "SELECT * FROM History WHERE membre_id = ? ORDER BY date_inscription DESC LIMIT 5";
+
+$stmt_history = $pdo->prepare($sql_history);
+$stmt_history->execute([$user_id]);
+$recent_history = $stmt_history->fetchall(PDO::FETCH_ASSOC);
 ?>
 
