c56d62af4d262609141c61710663e1560e398c23
Néhémie Mubenga Maweja <132246535+N3h3mya@users.noreply.github.com>
2024-07-17 17:39:22 +0200

    Update index.php
    removed the include header.php and $title to include the doctype section

--- php/index.php
+++ php/index.php
@@ -8,12 +8,30 @@ $sql = "SELECT * FROM Book ORDER BY date_publication DESC LIMIT 5";
 $stmt = $pdo->prepare($sql);
 $stmt->execute();
 $recent_books = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-$title = 'Accueil - Bibliothèque';
-include 'header.php';
-
 ?>
 
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil - Bibliothèque</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>Bienvenu sur votre bibliothèque en ligne</h1>
+        <nav>
+            <ul>
+                <li><a href="search.php">Rechercher des livres</a></li>
+                <li><a href="member.php">Gestion des membres</a></li>
+                <li><a href="borrow_reserve.php">Emprunter / Réserver</a></li>
+                <li><a href="history.php">Historique</a></li>
+                <li><a href="register.php">S'inscrire</a></li>
+                <li><a href="login.php">Se connecter</a></li>
+            </ul>
+        </nav>
+    </header>
     <main>
         <section id="latest_book">
             <h2>Derniers livres ajoutés</h2>
@@ -32,4 +50,6 @@ include 'header.php';
             </ul>
         </section>
     </main>
- <?php include 'footer.php'; ?>
+    <?php include 'footer.php'; ?>
+</body>
+</html>
