367c9cc0c90ead5ce1344aeebfbed9e1dc38d64d
Néhémie Mubenga Maweja <132246535+N3h3mya@users.noreply.github.com>
2024-07-20 15:31:21 +0200

    Update borrow_book.php
    Added functionality to update book availability

--- php/borrow_book.php
+++ php/borrow_book.php
@@ -14,4 +14,9 @@ $user_id = $_SESSION['user_id'];
 
 //Récupérer les infos du livre
 $book_id = $GET['book_id'];
+
+//Mise à jour de la disponiblité du livre 
+$sql_update_book = "UPDATE Book SET disponiblite = 0 WHER id = ?";
+$stmt_update_book = $pdo->prepare($sql_update_book);
+$stmt_update_book->execute([$book_id]);
 ?>
