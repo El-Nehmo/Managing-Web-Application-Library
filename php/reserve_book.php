f728508f537a45f4623315273fb9c69cf1e25e7d
Néhémie Mubenga Maweja <132246535+N3h3mya@users.noreply.github.com>
2024-07-22 17:03:38 +0200

    Update reserve_book.php
    Added the functionality of redirection if the user does not log in

--- php/reserve_book.php
+++ php/reserve_book.php
@@ -2,5 +2,9 @@
 session_start();
 require_once 'db_connection.php';
 
-
+//Redirection vers la page de connection si l'utilisateur n'est pas connecté
+if(!isset($_SESSION['user_id'])){
+    header('Location: login.php');
+    exit();
+}
 ?>
