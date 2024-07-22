986eacfaf230234ca0ea886f3a738239cf127466
Néhémie Mubenga Maweja <132246535+N3h3mya@users.noreply.github.com>
2024-07-22 21:44:38 +0200

    Update update_profile.php
    Added functionality to retrieve user info

--- php/update_profile.php
+++ php/update_profile.php
@@ -7,4 +7,14 @@ if(!isset($_SESSION['user_id'])){
     header('Location: login.php');
     exit();
 }
+
+//Récupérer les infos de l'utilisateur
+$user_id = $_SESSION['user_id'];
+$sql = "SEMECT FROM Membre WHERE id = ?";
+$stmt = $pdo->prepare($sql);
+$stmt->execute([$user_id]);
+$user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+$error_message = '';
+$succes_message = '';
 ?>
