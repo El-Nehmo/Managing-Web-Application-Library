c355129679c277ac8c7b50078a1e9afe9115e12b
Néhémie Mubenga Maweja <132246535+N3h3mya@users.noreply.github.com>
2024-07-23 18:00:11 +0200

    Update return_book.php
    Added functionality to update book availability

--- php/return_book.php
+++ php/return_book.php
@@ -7,4 +7,15 @@ if(!isset($_SESSION('iser_id'))){
     header('Location: login.php');
     exit();
 }
+
+//Récupérer l'id de l'utilisateur 
+$user_id = $_SESSION['user_id'];
+
+//Récupérer l'id du livre à retourner
+$book_id = $_GET['id'];
+
+//Mise à jour de la disponblité du livre 
+$sql_update_book = "UPDATE Book SET disponiblite = 1 WHERE id = ?";
+$stmt_update_book = $pdo->prepare($sql_update_book);
+$stmt_update_book->execute([$book_id]);
 ?>
